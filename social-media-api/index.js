87c9dd4ae2e435b4da4b7b97e0866312bca28930
Hate16 <tdendup166@gmail.com>
2026-05-19 22:43:03 +0600

    add social-media-api

--- social-media-api/index.js
+++ social-media-api/index.js
@@ -1,1 +1,20 @@
-console.log("Server is running!");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+app.use(helmet());
+
+app.get("/", (req, res) => {
+  res.send("Social Media API is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
